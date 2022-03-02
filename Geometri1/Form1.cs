29fe77f13d46050d950833c91c32b53bfc3ebab5
AmetoX <xametox.officiall@yahoo.com>
2022-03-02 09:58:55 +0200

    Geometrie

---
+++ Geometri1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geometri1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Blue, 5);
+            g.DrawLine(p, 0, 0, 100, 100);
+            g.DrawLine(p, 100, 100, 400, 100);
+            g.DrawLine(p, 400, 100, -900, 900);
+        }
+
+    }
+}
