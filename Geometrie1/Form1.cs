c481cca571a4c40dfb2a2586926c9886a8ae6c22
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-03-16 09:14:36 +0200

    Seminar

--- Geometrie1/Form1.cs
+++ Geometrie1/Form1.cs
@@ -18,21 +18,20 @@ namespace Geometrie1
         }  
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            //prima oara generam punctele si dupa generam dreptunghiul
             Graphics g = e.Graphics;
             Random random = new Random();
             Pen p = new Pen(Color.Blue, 5);
             Pen pc = new Pen(Color.Red, 5);            
-            g.DrawLine(p, 10, 10, 800, 10);
-            g.DrawLine(p, 800, 10, 800, 400);
-            g.DrawLine(p, 800, 400, 10, 400);
-            g.DrawLine(p, 10, 400, 10, 10);
-            //g.DrawRectangle(p, new Rectangle(10, 10, 800, 400));
-            for (int i = 0; i <= 10; i++)
+            
+            for (int i = 0; i < 10; i++)
             {
                 float x = random.Next(20, 780);
                 float y = random.Next(20, 380);
                 g.DrawEllipse(pc, x, y, 2, 2);
             }
+            //g.DrawRectangle(p, new Rectangle(10, 10, 800, 400));
+            //
         }
     }
 }
