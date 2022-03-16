cebed95bfa20c2de29f4c772c6bd3ad3c6034d86
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-03-16 09:35:08 +0200

    Seminar(3/16)

--- Geometrie1/Form1.cs
+++ Geometrie1/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Problema 1 (dreptunghi)
 namespace Geometrie1
 {
     public partial class Form1 : Form
--- Geometrie3/Form1.cs
+++ Geometrie3/Form1.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//Problema 2 (triunghi)
-namespace Geometrie2
+//Problema 3 (cerc)
+namespace Geometrie3
 {
     public partial class Form1 : Form
     {
