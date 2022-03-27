88c43d1e9d3e3c87a8543f7d59ec1d469b414c01
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-03-27 21:12:24 +0300

    prob3 cerc

--- Geometrie3/Form1.cs
+++ Geometrie3/Form1.cs
@@ -16,5 +16,53 @@ namespace Geometrie3
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Random random = new Random();
+            Pen p = new Pen(Color.Blue, 4);
+            Pen cr = new Pen(Color.Red, 4);
+            Pen rz = new Pen(Color.Green, 3);
+            float x=0, y=0,one = 0 ,two = 0;
+            float[] X = new float[9];
+            float[] Y = new float[9];
+            //points = points.OrderBy(item => item.X).ThenBy(item => item.Y).ToList();
+
+            for (int i = 0; i < 9; i++)
+            {
+                x = random.Next(panel1.Width-150);
+                y = random.Next(panel1.Height-150);
+                X[i] = x;
+                Y[i] = y;
+                g.DrawEllipse(cr, x, y, 2, 2);
+                if (x > one)
+                    one = x;
+                if (y > two)
+                    two = y;
+            }
+            int punct = random.Next(0, 9);
+            g.DrawEllipse(p, X[punct], Y[punct], 3, 3);
+            float distanta = 0;
+            //int pct = 0;
+            for(int i = 0; i < 9; i++)
+            {
+                float dis = (float)Math.Sqrt(Math.Pow(X[i] - X[punct], 2) +
+                                Math.Pow(Y[i] - Y[punct], 2));
+                if (dis > distanta)
+                {
+                    distanta = dis;
+                    //pct = i;
+                }
+            }
+            //distanta /= 2;
+            g.DrawEllipse(rz,X[punct]-distanta, Y[punct]-distanta, distanta + distanta, distanta + distanta);
+            //g.DrawEllipse(pen, centerX - radius, centerY - radius,radius + radius, radius + radius);
+        }
     }
 }
