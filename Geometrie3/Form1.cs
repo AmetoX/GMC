0e4148b713cba9267082be310fdaf6c659ceae76
AmetoX <67999655+AmetoX@users.noreply.github.com>
2022-03-30 07:58:06 +0300

    prob 3 cerc

--- Geometrie3/Form1.cs
+++ Geometrie3/Form1.cs
@@ -25,17 +25,17 @@ namespace Geometrie3
             Graphics g = e.Graphics;
             Random random = new Random();
             Pen p = new Pen(Color.Blue, 4);
-            Pen cr = new Pen(Color.Red, 4);
-            Pen rz = new Pen(Color.Green, 3);
+            Pen cr = new Pen(Color.DarkCyan, 4);
+            Pen rz = new Pen(Color.Black, 2);
             float x = 0, y = 0;
-            int n = random.Next(0, 100);
+            int n = random.Next(10, 20);
             float[] X = new float[n];
             float[] Y = new float[n];           
 
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < n; i++)
             {
-                x = random.Next(panel1.Width - 150);
-                y = random.Next(panel1.Height - 150);
+                x = random.Next(50,panel1.Width-100);
+                y = random.Next(50,panel1.Height-100);
                 X[i] = x;
                 Y[i] = y;
                 g.DrawEllipse(cr, x, y, 2, 2);
@@ -43,7 +43,8 @@ namespace Geometrie3
             float distanta1 = 0;
             float x1 = 0;
             float y1 = 0;
-
+            float one = 0;
+            float two = 0;
             for(int i = 0; i < n-1; i++)
             {
                 for(int j = 1; j < n; j++)
@@ -55,14 +56,27 @@ namespace Geometrie3
                         distanta1 = dis;
                         x1 = (X[j]+X[i])/2;
                         y1 = (Y[j]+Y[i])/2;
+                        one = X[j];
+                        two = Y[j];
                     }
                 }
             }
-            int one = (int)x1;
-            int two = (int)y1;
-            int distanta = (int)distanta1 / 2;
+            distanta1 /= 2;
+            for(int j = 0; j < n; j++)
+            {
+                float dis = (float)Math.Sqrt(Math.Pow(x1 - X[j], 2) +
+                Math.Pow(y1 - Y[j], 2));
+                if (dis > distanta1)
+                {
+                    distanta1 = dis;
+                }
+            }
+            //float distanta = (float)Math.Sqrt(Math.Pow(x1 - one, 2) +Math.Pow(y1 - two, 2));
+
+            float distanta = distanta1;
             g.DrawEllipse(rz, x1, y1, 2, 2);
-            g.DrawEllipse(rz,one-distanta, two-distanta,distanta + distanta+10, distanta + distanta+10);
+            //g.DrawEllipse(rz, x1 - distanta, y1 - distanta, distanta + distanta + 15, distanta + distanta + 15);
+            g.DrawEllipse(rz,x1-distanta, y1-distanta,distanta + distanta+5, distanta + distanta+5);
             //g.DrawEllipse(pen, centerX - radius, centerY - radius,radius + radius, radius + radius);
             //points = points.OrderBy(item => item.X).ThenBy(item => item.Y).ToList();
         }
